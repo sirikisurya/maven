3f3459dd8178ce80398f5051554203c9b3894d12
sirikisurya <109733819+sirikisurya@users.noreply.github.com>
2023-05-05 11:07:44 +0530

    commit

---
+++ webapp/src/main/java/pages/basePage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class basePage {
+    WebDriver driver;
+    basePage(WebDriver driver)
+    {
+        this.driver=driver;
+    }
+}
